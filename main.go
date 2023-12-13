605a39f14d630817849892c4a10e33cc598b4250
vizoss <think220216@gmail.com>
2023-12-13 22:06:15 +0800

    update

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"github.com/thk-im/thk-im-base-server/conf"
+	"github.com/thk-im/thk-im-contact-server/pkg/app"
+	"github.com/thk-im/thk-im-contact-server/pkg/handler"
+)
 
+func main() {
+	configPath := "etc/contact_server.yaml"
+	config := conf.LoadConfig(configPath)
+
+	appCtx := &app.Context{}
+	appCtx.Init(config)
+	handler.RegisterContactApiHandlers(appCtx)
+
+	appCtx.StartServe()
 }
---
+++ pkg/handler/contact_handler.go
@@ -0,0 +1,7 @@
+package handler
+
+import "github.com/thk-im/thk-im-contact-server/pkg/app"
+
+func RegisterContactApiHandlers(appCtx *app.Context) {
+
+}
