6cb4c934b61a24792a4edbe8772ba38ed96a9f94
vizoss <think220216@gmail.com>
2024-03-31 23:57:32 +0800

    add flag

--- pkg/dto/session.go
+++ pkg/dto/session.go
@@ -6,18 +6,19 @@ type CreateSessionReq struct {
 }
 
 type CreateSessionResp struct {
-	SId      int64   `json:"s_id"`
-	ParentId int64   `json:"parent_id"`
-	EntityId int64   `json:"entity_id"`
-	Type     int     `json:"type"`
-	Name     string  `json:"name"`
-	Remark   string  `json:"remark"`
-	ExtData  *string `json:"ext_data"`
-	Mute     int     `json:"mute"`
-	Role     int     `json:"role"`
-	CTime    int64   `json:"c_time"`
-	MTime    int64   `json:"m_time"`
-	Top      int64   `json:"top"`
-	Status   int     `json:"status"`
-	IsNew    bool    `json:"is_new"` // 如果之前已经创建，false
+	SId          int64   `json:"s_id"`
+	ParentId     int64   `json:"parent_id"`
+	EntityId     int64   `json:"entity_id"`
+	Type         int     `json:"type"`
+	Name         string  `json:"name"`
+	Remark       string  `json:"remark"`
+	ExtData      *string `json:"ext_data"`
+	Mute         int     `json:"mute"`
+	Role         int     `json:"role"`
+	CTime        int64   `json:"c_time"`
+	MTime        int64   `json:"m_time"`
+	Top          int64   `json:"top"`
+	FunctionFlag int64   `json:"function_flag"`
+	Status       int     `json:"status"`
+	IsNew        bool    `json:"is_new"` // 如果之前已经创建，false
 }
